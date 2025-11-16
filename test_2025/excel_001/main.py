86a3ed54469a7555877dcaf1a72452178af84f1c
young <2963813539@qq.com>
2025-11-16 15:55:31 +0800

    创建一维插值模型

--- test_2025/excel_001/main.py
+++ test_2025/excel_001/main.py
@@ -8,7 +8,18 @@ def linear_regression_prediction():
     arr1=[1,2,3,4,5]
     arr2=[13,12,20,30,31]
 
-
+# 创建一维插值模型
+from scipy.interpolate import interp1d
+import numpy as np
+# 已知数据点
+x = np.array([0, 1, 2, 3, 4])
+y = np.array([0, 2, 4, 6, 8])
+# 创建插值函数
+f = interp1d(x, y, kind='linear')
+# 插值计算
+x_new = 2.5
+y_new = f(x_new)
+print(f"插值后的值为: {y_new}")
 
 def main():
     ex_1=read_and_process_excel("temp/西红柿.xlsx")
