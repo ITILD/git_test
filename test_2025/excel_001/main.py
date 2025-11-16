b4e74d76a129b679722b637866481054da24e029
young <2963813539@qq.com>
2025-11-16 16:13:36 +0800

    命名每一个步骤

--- test_2025/excel_001/main.py
+++ test_2025/excel_001/main.py
@@ -3,12 +3,19 @@ import polars as pl
 import datetime as dt
 from sklearn.linear_model import LinearRegression
 
+
+# 线性回归预测  
 def linear_regression_prediction():
 
     arr1=[1,2,3,4,5]
     arr2=[13,12,20,30,31]
 
+
+
 # 创建一维插值模型
+def linear_interpolation(x, y):
+    f = interp1d(x, y, kind='linear')
+    return f
 from scipy.interpolate import interp1d
 import numpy as np
 # 已知数据点
@@ -21,10 +28,12 @@ x_new = 2.5
 y_new = f(x_new)
 print(f"插值后的值为: {y_new}")
 
+
+# 主函数
 def main():
     ex_1=read_and_process_excel("temp/西红柿.xlsx")
 
-
+# 读取Excel文件并预处理
 def read_and_process_excel(file_path: str) -> pl.DataFrame:
     df = pl.read_excel(file_path)
     result = df.to_dicts()
