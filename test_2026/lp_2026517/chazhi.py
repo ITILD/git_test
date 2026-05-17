671f70cfecbc16e3b164fbeb0ff5d30d7f093f04
JiananP <1242122759@qq.com>
2026-05-17 15:41:00 +0800

    插入数据

--- test_2026/lp_2026517/chazhi.py
+++ test_2026/lp_2026517/chazhi.py
@@ -1,13 +1,15 @@
-import pandas as pd
 import numpy as np
+from scipy.interpolate import CubicSpline
 
-# 创建一个有缺失值的示例数据
-df = pd.DataFrame({
-    'A': [1, 2, np.nan, 4, 5],
-    'B': [10, np.nan, 30, 40, 50]
-})
-
-# 用每列的均值填充缺失值
-df_filled = df.fillna(df.mean())
-
-print(df_filled)
+def data(x_known, y_known, month):
+   
+    cs = CubicSpline(x_known, y_known)
+    return cs(month)
+    
+    
+if __name__ == "__main__":
+    x_known = np.array([1, 2, 3, 5, 6])
+    y_known = np.array([10, 11, 13, 20, 25])
+    x_full = np.arange(1, 7)
+    y_full = data(x_known, y_known, x_full)
+    print(y_full)                   
--- test_2026/lp_2026517/main.py
+++ test_2026/lp_2026517/main.py
@@ -1,6 +1,10 @@
+import numpy as np
+from scipy.interpolate import CubicSpline
+from chazhi import data
+
+
 def main():
     print("Hello from lp-2026517!")
 
-
-if __name__ == "__main__":
+if __name__ == "__main__":               
     main()
